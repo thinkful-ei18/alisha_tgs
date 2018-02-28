4433d87dc9eceecad5f4c157aec2d728aef956ed
alishaevn <alieva.inc@gmail.com>
2018-02-28 08:59:11 -0800

    landing page calls second page

--- src/TGS.js
+++ src/TGS.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import './TGS.css';
 
 export default class TGS extends Component {
-  render() {
-    return (
-      <div className="TGS">
-  
-      </div>
-    );
+
+  constructor() {
+    super();
+
+    this.state= {
+      clicked: false
+    }
   }
+
+  startButton(e) {
+    this.setState({clicked: true})
+  }
+
+  render() {
+
+    if (!this.state.clicked) {
+      return (
+        <div className="TGS">
+          <h1>The Greatest Showman</h1>
+          <h4>Starring: Hugh Jackman, Michelle Williams, Zac Efron & Zendaya</h4>
+          <button onClick={ e => this.startButton(e) }>Start</button>
+        </div>
+      );
+    }
+
+    return <PageOne />
+  }
+
 }
 
+function PageOne() {
+
+  return (
+    <div className="PageOne">
+      <h1>Have you seen it?</h1>
+    </div>
+    )
+
+}
