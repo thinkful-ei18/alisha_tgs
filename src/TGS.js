87b877fdb47e83c83d98dd64dca1a6684fd9de76
alishaevn <alieva.inc@gmail.com>
2018-02-28 10:50:13 -0800

    probably about to break something so I am just gonna commit this

--- src/TGS.js
+++ src/TGS.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import './TGS.css';
 
+
 export default class TGS extends Component {
 
   constructor() {
     super();
 
     this.state= {
-      clicked: false
+      clicked: false,
+      yesButtonClick: false,
+      noButtonClick: false
     }
+
+    this.yesButton = this.yesButton.bind(this);
+    this.noButton = this.noButton.bind(this);
   }
+
+
 
   startButton(e) {
     this.setState({clicked: true})
+  }
+
+  yesButton(e) {
+    this.setState({ yesButtonClick: true })
+  }
+
+  noButton(e) {
+    this.setState({ noButtonClick: true })
   }
 
   render() {
@@ -27,17 +43,85 @@ export default class TGS extends Component {
       );
     }
 
-    return <PageOne />
+    return <QuestionOne />
   }
+}
+
+class QuestionOne extends Component {
+
+  constructor() {
+    super();
+    this.yesButton = this.yesButton.bind(this);
+    this.noButton = this.noButton.bind(this);
+  }
+
+  render() {
+    if (this.state.yesButtonClick) {
+      return <QuestionTwo />
+    } else if (this.state.noButtonClick) {
+      return <GoNow />
+    }
+    return (
+      <div className="QuestionOne">
+        <h1>Have you seen it?</h1>
+        <YesButton />
+        <NoButton />
+      </div>
+    )
+  }
+
+
+//   if(this.state.yesButtonClick===undefined) {
+//     return (
+//       <div className="QuestionOne">
+//         <h1>Have you seen it?</h1>
+//         <YesButton />
+//         <NoButton />
+//       </div>
+//       )
+//   } else if(this.state.yesButtonClick) {
+//     return <QuestionTwo />
+//   } else if (this.state.noButtonClick) {
+//     return <GoNow />
+//   } 
+// }
 
 }
 
-function PageOne() {
+function YesButton() {
 
   return (
-    <div className="PageOne">
-      <h1>Have you seen it?</h1>
-    </div>
-    )
+    <span className="button">
+      <button onClick={e => this.yesButton(e)}>yes</button>
+    </span>
+  )
+}
 
+function NoButton() {
+
+  return (
+    <span className="button">
+      <button onClick={e => this.noButton(e)}>no</button>
+    </span>
+  )
+}
+
+function QuestionTwo() {
+
+  return (
+    <div className="QuestionTwo">
+      <h1>Did you like it></h1>
+      <YesButton />
+      <NoButton />
+    </div>
+  )
+}
+
+function GoNow() {
+
+  return (
+    <div className="GoNow">
+      <h1>Stop what you're doing and go buy a ticket!</h1>
+    </div>
+  )
 }
