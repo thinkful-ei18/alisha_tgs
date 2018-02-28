5dc42d452d9babad32f01c757e1a3668e6412011
alishaevn <alieva.inc@gmail.com>
2018-02-28 08:06:58 -0800

    updated all references of "App" to become "TSG"

---
+++ src/TGS.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TGS.css';
+
+export default class TGS extends Component {
+  render() {
+    return (
+      <div className="TGS">
+  
+      </div>
+    );
+  }
+}
+
